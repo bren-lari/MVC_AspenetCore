a2db2601f707b2e3cbb536c20d748219eb17f234
DEV <dev@info.local>
2021-01-08 16:16:19 -0300

    Alterações feitas por mim na parte de css e html do site em c#.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPENET_Aula.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
